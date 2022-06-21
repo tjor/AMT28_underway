49ce53bda925aba5893d656450f4b07ad6f16403
grg <gdal@pml.ac.uk>
2022-06-21 11:04:11 +0100

    deal with empty acs structures

--- Underway/chlacs.m
+++ Underway/chlacs.m
@@ -6,7 +6,7 @@ function chla = chlacs(acs)
 # acs contains at least acs.wv and acs.ap
 #
 
-
+  if ~all(isnan(acs.ap))
 
     wv650 = find(acs.wv==650); %  absorption at left of peak
     wv676 = find(acs.wv==676); % peak absoprtion wavelength
@@ -14,6 +14,9 @@ function chla = chlacs(acs)
 
     % peak height relative to edges of peak re-scaled to Chl (65 used in-place of 64 wv units - legacy mistake - no concern) 
     chla = (acs.ap(:,wv676) -39/65.*acs.ap(:,wv650)-26/65*acs.ap(:,wv714))./0.014; % conversion factor 0.014 m^2 mg^-1 of Chl
-    
+   
+   else
+    chla = nan(1440,1);
+   endif
 endfunction    
 
